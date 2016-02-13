4c8e2c44ac58f11b9a6c74a71feedcd98845e61b
axelg1993 <axelg1993@gmail.com>
2016-02-13 18:11:53 -0500

    Updated poster image code

--- Flicks-TONY/MoviesViewController.swift
+++ Flicks-TONY/MoviesViewController.swift
@@ -99,25 +99,23 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         
         let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! Movie_Cell
-        
         let movie = movies![indexPath.row]
         let title = movie ["title"] as! String
         let overview = movie ["overview"] as! String
-        let posterPath = movie ["poster_path"] as! String
-        
+       
+     if let posterPath = movie ["poster_path"] as? String{
         let baseUrl = "http://image.tmdb.org/t/p/w500"
         let imageUrl = NSURL(string: baseUrl + posterPath)
-       
+            cell.posterView.setImageWithURL(imageUrl!)}
         
+     else {cell.posterView.image = nil}
+
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
-        cell.posterView.setImageWithURL(imageUrl!)
         
-    
         print ("row \(indexPath.row)")
         return cell
-    }
-
+        }
 
 
 
