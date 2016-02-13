c26f948597d8c4a0eec7517a2ea308685d7ddf4f
axelg1993 <axelg1993@gmail.com>
2016-02-13 16:34:56 -0500

    My Required portion is done

--- Flicks-TONY/MoviesViewController.swift
+++ Flicks-TONY/MoviesViewController.swift
@@ -8,31 +8,61 @@
 
 import UIKit
 import AFNetworking
-class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+import MBProgressHUD
 
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [NSDictionary]?
-    
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+      // here went the let refressh control
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
         tableView.dataSource = self
         tableView.delegate = self
-        
-        
-        
-        
-        
-        
-        
-        
+
         // Do any additional setup after loading the view.
+            
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = NSURLRequest(
+            URL: url!,
+            cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData,
+            timeoutInterval: 10)
     
+        
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate: nil,
+            delegateQueue: NSOperationQueue.mainQueue()
+        )
+        
+    MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        
+        
+        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+
+    MBProgressHUD.hideHUDForView(self.view, animated: true)
+       
+        if let data = dataOrNil {
+        if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            print("response: \(responseDictionary)")
+                            
+                            
+           self.movies = responseDictionary["results"] as?  [NSDictionary]
+           self.tableView.reloadData()}}})
+        task.resume()
+    }
     
-    
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
         let request = NSURLRequest(
@@ -42,33 +72,28 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         let session = NSURLSession(
             configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
-            delegate: nil,
-            delegateQueue: NSOperationQueue.mainQueue()
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
         )
         
-        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
-            completionHandler: { (dataOrNil, response, error) in
-                if let data = dataOrNil {
-                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
-                        data, options:[]) as? NSDictionary {
-                            print("response: \(responseDictionary)")
-                            
-           self.movies = responseDictionary["results"] as?  [NSDictionary]
-           self.tableView.reloadData()
-                    }
-                }
-        })
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (data, response, error) in
+                
+                self.tableView.reloadData()
+                refreshControl.endRefreshing()
+        });
         task.resume()
-
-    
-    
-    
     }
-
+ 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         if let movies = movies {
             return movies.count
@@ -79,6 +104,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
         let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! Movie_Cell
         
         let movie = movies![indexPath.row]
@@ -87,10 +113,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let posterPath = movie ["poster_path"] as! String
         
         let baseUrl = "http://image.tmdb.org/t/p/w500"
-        
         let imageUrl = NSURL(string: baseUrl + posterPath)
        
-        
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
@@ -99,8 +123,10 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
         print ("row \(indexPath.row)")
         return cell
-    
     }
+
+
+
 
     /*
     // MARK: - Navigation
--- Podfile
+++ Podfile
@@ -1,9 +1,11 @@
 # Uncomment this line to define a global platform for your project
-# platform :ios, '8.0'
+ platform :ios, '8.0'
 # Uncomment this line if you're using Swift
 use_frameworks!
 
 target 'Flicks-TONY' do
 pod 'AFNetworking'
 end
+pod 'MBProgressHUD'
 
+inhibit_all_warnings!
